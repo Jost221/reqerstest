fc492ac002d538f052c92874973314f7e0395b9d
Jost221 <mr.z.75@mail.ru>
2023-12-26 00:57:28 +0300

    init

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,79 +1,106 @@
+import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.equalTo;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
 
 public class Example {
-    @Test(description = "check working allure и Jenkins")
+    @Test(description = "Проверка работоспособности allure и Jenkins")
     public void testAllureAndJenkins() {
         assertEquals(1, 1);
     }
 
-    @Test(description = "GET DATA SINGLE USER")
-    public void testListUsers() {
-        baseURI = "https://reqres.in/api";
+    @Test(description = "Авторизация не зарегестрированного пользователя")
+    public void testUnregisteredUserAuthorization() {
+        baseURI = "http://localhost:4401/api/v1";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("login", "login");
+        requestParams.put("password", "password");
+        requestParams.put("role", "EXPLORER");
+
         given()
-                .param("page", 2)
+                .contentType(ContentType.JSON)
+                .body(requestParams.toJSONString())
         .when()
-                .get("/users/2")
-                .then()
-                .body("data.id", equalTo(2))
-                .body("data.email", equalTo( "janet.weaver@reqres.in"))
-                .body("data.first_name", equalTo("Janet"))
-                .body("data.last_name", equalTo("Weaver"))
-                .body("data.avatar",  equalTo("https://reqres.in/img/faces/2-image.jpg"))
+                .post("/auth/login/")
+        .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/not-found-scheme.json"))
+                .body("errorCode", equalTo("Not Found"))
+                .body("errorMessage", equalTo("Бортовой компьютер не смог вас идентифицировать"))
+                .statusCode(404).log().all();
+    }
+
+    @Test(description = "Авторизация пользователя с несуществующей ролью")
+    public void testNonExistentRoleUserAuthorization() {
+        baseURI = "http://localhost:4401/api/v1";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("login", "user");
+        requestParams.put("password", "user");
+        requestParams.put("role", "Hitler");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestParams.toJSONString())
+        .when()
+                .post("/auth/login/")
+        .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/bad-request-schema.json"))
+                .body("errorCode", equalTo("Bad Request"))
+                .body("errorMessage", equalTo("Данная роль недоступна"))
+                .statusCode(400).log().all();
+    }
+
+    @Test(description = "Получение всех галактик")
+    public void testGetAllGalaxies() {
+        baseURI = "http://localhost:4401/api/v1";
+
+        given()
+                .param("detailed","true")
+        .when()
+                .get("/galaxy-app/galaxies/")
+        .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/all-galaxies-schema.json"))
+                .body("galaxyId", hasItems(1, 2, 3))
+                .body("galaxyName", hasItems("Автоматизация тестирования", "UI Дизайн", "Frontend Разработка"))
                 .statusCode(200).log().all();
     }
 
-    @Test(description = "create user")
-    public void testCreateUser() {
-        baseURI = "https://reqres.in/api";
-
+    @Test(description = "Выход авторизированного пользователя")
+    public void testAuthorizedUserExit() {
+        baseURI = "http://localhost:4401/api/v1";
         JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "morpheus");
-        requestParams.put("job", "leader");
+        requestParams.put("login", "user");
+        requestParams.put("password", "user");
+        requestParams.put("role", "EXPLORER");
 
         given()
-                .header("Content-Type", "application/json")
                 .body(requestParams.toJSONString())
         .when()
-                .post("/users")
+                .post("/auth/logout/")
         .then()
-                .body("name", equalTo("morpheus"))
-                .body("job", equalTo("leader"))
-                .statusCode(201).log().all();
-    }
-
-    @Test(description = "update user with PUT")
-    public void testUpdateUserPUT() {
-        baseURI = "https://reqres.in/api";
-
-        JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "morpheus");
-        requestParams.put("job", "zion resident");
-
-        given()
-                .header("Content-Type", "application/json")
-                .body(requestParams.toJSONString())
-        .when()
-                .put("/users/2")
-        .then()
-                .body("name", equalTo("morpheus"))
-                .body("job", equalTo("zion resident"))
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/logout-success-schema.json"))
+                .body("message", equalTo("Выход успешный"))
                 .statusCode(200).log().all();
     }
 
-    @Test(description = "DELETE USER")
-    public void testDeleteUser() {
-        baseURI = "https://reqres.in/api";
+    @Test(description = "Получение систем галактики")
+    public void testGetAllSystemsOfGalaxy() {
+        baseURI = "http://localhost:4401/api/v1";
 
-        delete("/users/2")
-                .then()
-                .statusCode(204).log().all();
+        get("/galaxy-app/galaxies/1")
+        .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/systems-of-galaxy-schema.json"))
+                .body("galaxyId", equalTo(1))
+                .body("galaxyName", equalTo("Автоматизация тестирования"))
+                .body("orbitList.orbitId", hasItems(1, 2, 3, 4))
+                .statusCode(200).log().all();
     }
-
 }
